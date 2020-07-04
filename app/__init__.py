921ff68b227e156abd236bd65cc6c566cb8fce16
Flarinli <flarinli@rambler.ru>
2020-07-04 11:13:57 +0300

    chapter_4

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from config import Config
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)  # noqa
 app.config.from_object(Config)  # noqa
+db = SQLAlchemy(app)  # noqa
+migrate = Migrate(app, db)  # noqa
 
-from app import routes
+from app import (routes, models)
